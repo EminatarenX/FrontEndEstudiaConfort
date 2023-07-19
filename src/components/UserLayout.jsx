a1674414771d225240d46b4779dbdeaa2cb8afd7
EminatarenX <eminataren2002@gmail.com>
2023-07-19 17:03:19 -0600

    links to dashboard and main page

--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -20,15 +20,15 @@ function UserLayout() {
   return (
     <>  
       <aside className={`lg:hidden fixed flex flex-col ${aside? 'right-0':' -right-[400px]'} bg-slate-950 px-32 gap-10 h-full pt-20 transition-all duration-700 ease  z-10`}>
-        <Link className="text-white text-xl">Estudia<span className="text-sky-400">Confort</span></Link>
+        <Link to='/dashboard' className="text-white text-xl">Estudia<span className="text-sky-400">Confort</span></Link>
         <Link onClick={()=> setAside(false)} to='/dashboard' className="text-white text-xl">Habitaciones</Link>
         <Link onClick={()=> setAside(false)} to='/dashboard/perfil' className="text-white text-xl">Perfil</Link>
         <button onClick={cerrarSesion} className="text-white text-xl">Cerrar Sesión</button>
       </aside>
       <header className=" flex justify-between px-8 h-[100px] items-center bg-gradient-to-r from-slate-950 to-slate-800">
-      <h1 className="text-4xl text-white  animate-entrada  ">
+      <Link to='/dashboard' className="text-4xl text-white  animate-entrada  ">
           Estudia<span className="text-cyan-700">Confort</span>
-        </h1>
+        </Link>
         <nav className="flex gap-10 animate-entrada">
           <Link to='/dashboard' className="hidden lg:flex text-md text-white border border-sky-200 hover:bg-slate-600 transition-all hover:-translate-y-1 h-12 w-[150px] justify-center items-center rounded font-semibold">Habitaciones</Link>
           <Link to='/dashboard/perfil' className="hidden lg:flex text-md text-white border border-sky-200 hover:bg-slate-600 transition-all hover:-translate-y-1 h-12 w-[150px] justify-center items-center rounded font-semibold">Perfil</Link>
